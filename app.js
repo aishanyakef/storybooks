70020c21a9a445c11136c90e1379c4985a943329
Hassan Yakefujiang <yhassan@vt.edu>
2018-04-19 17:04:48 -0400

    handled off

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Passport config
+require('./config/passport')(passport);
+
+// Load Routes
+const auth = require('./routes/auth');
 
 const app = express();
+
+app.get('/', (req, res) => {
+   res.send('Hello World');
+});
+
+// Use Routes
+app.use('/auth', auth);
+
 const port = process.env.PORT || 5000;
+
 
 app.listen(port, () => {
    console.log(`Server started on port ${port}`);
 });
 
-app.get('/', (req, res)=> {
-   res.send('Hello World');
-});
+
